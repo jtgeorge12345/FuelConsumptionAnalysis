2fa40771f3a45b7fe4d8643e6fe69df3874c0ada
jgeorge <jasontgeorge1@gmail.com>
2019-02-04 21:18:18 -0800

    Initial commit. Found data source and set up repository.

---
+++ FuelConsumptionAnalysis.py
@@ -0,0 +1,21 @@
+#Fuel Consumption Analysis
+
+#Data Source: https://open.canada.ca/data/en/dataset/98f1a129-f628-4ce4-b24d-6f16bf24dd64
+import pandas
+
+
+# read in CSV file
+
+#Put data into a Pandas Dataframe
+
+#Use learning techniques to predict fuel consumption based on other attributes
+
+#Cluster analysis and create plots of clusters
+
+#compare clusters to actuals
+
+#Which cars improved the most over 15 years?
+
+#Which cars are improving the most consistently?
+
+#
