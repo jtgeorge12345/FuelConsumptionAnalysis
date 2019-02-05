e2b58a96e5d5775bd0d7c7a7065d7d41289e13aa
jgeorge <jasontgeorge1@gmail.com>
2019-02-04 22:43:38 -0800

    Added inital loading of data and printing some descriptive statistics

--- FuelConsumptionAnalysis.py
+++ FuelConsumptionAnalysis.py
@@ -1,10 +1,19 @@
 #Fuel Consumption Analysis
-
 #Data Source: https://open.canada.ca/data/en/dataset/98f1a129-f628-4ce4-b24d-6f16bf24dd64
-import pandas
 
+import pandas as pd
+pd.set_option("display.max_columns", 13)
 
 # read in CSV file
+data = pd.read_csv("Canada Fuel Consumption Ratings 2000-2014 2.csv")
+print(data.head())
+
+data.dropna(axis=1, inplace=True, thresh=2000)
+print(data.head())
+
+print(data.describe())
+
+print(data.loc[data['CYLINDERS'] == 16])
 
 #Put data into a Pandas Dataframe
 
@@ -12,10 +21,12 @@ import pandas
 
 #Cluster analysis and create plots of clusters
 
-#compare clusters to actuals
+#compare clusters to actual car categories
 
 #Which cars improved the most over 15 years?
 
 #Which cars are improving the most consistently?
 
-#
+#Which cars are getting worse?
+
+#Can cost data be added to this dataset?
